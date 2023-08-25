d827cb6db4567967e607a8c56ea7022e6d89a15e
strikejet <strikejet@gmail.com>
2023-08-25 12:42:31 +0530

    small changes for crossorigin

--- EMS_updated/src/main/java/com/app/controller/AnnouncementController.java
+++ EMS_updated/src/main/java/com/app/controller/AnnouncementController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import com.app.entities.Department;
 import com.app.service.AnnouncementService;
 import com.app.service.DepartmentService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/announcements")
 public class AnnouncementController {
--- EMS_updated/src/main/java/com/app/controller/EmployeeController.java
+++ EMS_updated/src/main/java/com/app/controller/EmployeeController.java
@@ -6,7 +6,9 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -22,6 +24,7 @@ import com.app.service.EmployeeService;
 
 import org.springframework.web.bind.annotation.RequestBody;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
